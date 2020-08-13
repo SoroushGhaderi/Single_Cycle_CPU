ca2199b096d97c3f1ebded856568b0b5fe815a57
Soroush <ghaderi.soroush1995@gmail.com>
2020-08-13 17:00:48 +0430

    add multiplexer 5bit

---
+++ multiplexer_5bit_2_1.vhd
@@ -0,0 +1,26 @@
+-- 5-bit wide 2:1 multiplexer
+
+library ieee;
+use ieee.std_logic_1164.all;
+
+
+entity multiplexer_5bit_2_1 is
+    port(
+        signal SEL : in std_logic;
+        signal A, B : in std_logic_vector(4 downto 0);
+        signal O : out std_logic_vector(4 downto 0)
+    );
+end entity multiplexer_5bit_2_1;
+
+
+architecture behavior of multiplexer_5bit_2_1 is
+    begin
+        process(A, B, SEL)
+            begin
+                if SEL = '0' then
+                    O <= A;
+                elsif SEL = '1' then
+                    O <= B;
+                end if;
+        end process;
+end architecture behavior;
